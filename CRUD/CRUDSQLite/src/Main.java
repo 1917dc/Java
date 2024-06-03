472493b63e145908873d7466e06f6d5cf4d6d7a0
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-06-03 20:48:06 -0300

    algumas funções feitas

--- CRUD/CRUDSQLite/src/Main.java
+++ CRUD/CRUDSQLite/src/Main.java
@@ -1,3 +1,15 @@
-public class Main {
+import controller.DatabaseControl;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> generos = new ArrayList();
+
+        generos.add(6);
+        var db = new DatabaseControl();
+        db.insertMovie("The Social Network", "2010", 1, generos);
+    }
 }
--- CRUD/CRUDSQLite/src/controller/DatabaseControl.java
+++ CRUD/CRUDSQLite/src/controller/DatabaseControl.java
@@ -1,20 +1,85 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseControl {
-    private static final String path = "jdbc:sqlite:moviesDB.sqlite3";
-    private Statement stmt;
-    public void connect(){
-        Connection conn;
+    private static final String path = "jdbc:sqlite:moviesDB.db";
+    private Connection conn = null;
+
+    public void selectAllMovies(String tableName) {
         try {
             conn = DriverManager.getConnection(path);
+            Statement stmt = conn.createStatement();
+            String selectAllSQL = MessageFormat.format("SELECT * FROM {0}", tableName);
+            ResultSet rsFilme = stmt.executeQuery(selectAllSQL);
+
+
+            while (rsFilme.next()) {
+                String selectDiretoresSQL = MessageFormat.format("""
+                        SELECT diretor.nome FROM Filme filme 
+                        JOIN Diretor diretor 
+                        ON filme.diretor_id = diretor.id
+                        WHERE filme.id = {0};
+                        """, rsFilme.getRow());
+
+                Statement stmtDiretor = conn.createStatement();
+                ResultSet rsDiretor = stmtDiretor.executeQuery(selectDiretoresSQL);
+
+                System.out.println("------------------------------------------");
+                System.out.println("Filme: " + rsFilme.getString("titulo"));
+                System.out.println("Ano: " + rsFilme.getString("ano"));
+                System.out.println("Diretor: " + rsDiretor.getString("nome"));
+                System.out.println("-----------------------------------------");
+
+
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            closeConnection(conn);
         }
     }
-    
+
+    public void insertMovie(String titulo, String ano, int diretor_id, List<Integer> generos) {
+        try {
+            String insertMovieSQL = "INSERT INTO Filme(titulo, ano, diretor_id) VALUES(?,?,?)";
+            conn = DriverManager.getConnection(path);
+            PreparedStatement pstmtFilme = conn.prepareStatement(insertMovieSQL);
+            pstmtFilme.setString(1, titulo);
+            pstmtFilme.setString(2, ano);
+            pstmtFilme.setInt(3, diretor_id);
+            pstmtFilme.executeUpdate();
+
+            Statement stmt = conn.createStatement();
+            String selectFilmeSQL = String.format("SELECT id FROM Filme WHERE titulo = '%s';", titulo);
+
+            String insertFilmeGenero = "INSERT INTO Filme_Genero(genero_id, filme_id) VALUES(?,?)";
+            PreparedStatement pstmtGeneroFilme = conn.prepareStatement(insertFilmeGenero);
+            for (Integer generosInsert : generos) {
+                ResultSet rs = stmt.executeQuery(selectFilmeSQL);
+                pstmtGeneroFilme.setInt(1, generosInsert);
+                pstmtGeneroFilme.setInt(2, rs.getInt("id"));
+                pstmtGeneroFilme.executeUpdate();
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection(conn);
+        }
+    }
+
+    private void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
